8691e9d8d6513c99634e527d0966901b294f5895
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-28 22:41:27 +0000

    Convert tabs to spaces

--- src/hooks.ts
+++ src/hooks.ts
@@ -3,34 +3,34 @@ import { v4 as uuid } from '@lukeed/uuid';
 import type { GetContext, Handle } from '@sveltejs/kit';
 
 export const getContext: GetContext = (request) => {
-	const cookies = cookie.parse(request.headers.cookie || '');
+  const cookies = cookie.parse(request.headers.cookie || '');
 
-	return {
-		is_new: !cookies.userid,
-		userid: cookies.userid || uuid()
-	};
+  return {
+    is_new: !cookies.userid,
+    userid: cookies.userid || uuid()
+  };
 };
 
 export const handle: Handle = async ({ request, render }) => {
-	// TODO https://github.com/sveltejs/kit/issues/1046
-	const response = await render({
-		...request,
-		method: (request.query.get('_method') || request.method).toUpperCase()
-	});
+  // TODO https://github.com/sveltejs/kit/issues/1046
+  const response = await render({
+    ...request,
+    method: (request.query.get('_method') || request.method).toUpperCase()
+  });
 
-	const { is_new, userid } = request.context;
+  const { is_new, userid } = request.context;
 
-	if (is_new) {
-		// if this is the first time the user has visited this app,
-		// set a cookie so that we recognise them when they return
-		return {
-			...response,
-			headers: {
-				...response.headers,
-				'set-cookie': `userid=${userid}; Path=/; HttpOnly`
-			}
-		};
-	}
+  if (is_new) {
+    // if this is the first time the user has visited this app,
+    // set a cookie so that we recognise them when they return
+    return {
+      ...response,
+      headers: {
+        ...response.headers,
+        'set-cookie': `userid=${userid}; Path=/; HttpOnly`
+      }
+    };
+  }
 
-	return response;
+  return response;
 };
