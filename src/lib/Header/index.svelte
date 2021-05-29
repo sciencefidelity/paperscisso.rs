afa5ecc9eb9fa3fbf602dce79f4f2036104b245e
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-05-29 00:27:04 +0000

    Change the header to match the sketch file

--- src/lib/Header/index.svelte
+++ src/lib/Header/index.svelte
@@ -1,120 +1,81 @@
 <script lang="ts">
-  import { page } from '$app/stores';
   import logo from './svelte-logo.svg';
 </script>
 
 <header>
-  <div class="corner">
-    <a href="https://kit.svelte.dev">
-      <img src={logo} alt="SvelteKit" />
-    </a>
-  </div>
-
-  <nav>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L1,2 C1.5,3 1.5,3 2,3 L2,0 Z" />
-    </svg>
-    <ul>
-      <li class:active={$page.path === '/'}><a sveltekit:prefetch href="/">Home</a></li>
-      <li class:active={$page.path === '/about'}><a sveltekit:prefetch href="/about">About</a></li>
-      <li class:active={$page.path === '/todos'}><a sveltekit:prefetch href="/todos">Todos</a></li>
-    </ul>
-    <svg viewBox="0 0 2 3" aria-hidden="true">
-      <path d="M0,0 L0,3 C0.5,3 0.5,3 1,2 L2,0 Z" />
-    </svg>
-  </nav>
-
-  <div class="corner">
-
+  <div class="container">
+    <div class="scissors noselect">&#9985;</div>
+    <div class="logo-name noselect">Paper<br />Scissors</div>
+    <div class="intro">
+      <p><span class="drop-cap">L</span>orem ipsum dolor sit amet, consectetur adipisici elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+    </div>
   </div>
 </header>
 
-<style>
-  header {
-    display: flex;
-    justify-content: space-between;
-  }
-
-  .corner {
-    width: 3em;
-    height: 3em;
-  }
-
-  .corner a {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 100%;
-  }
-
-  .corner img {
-    width: 2em;
-    height: 2em;
-    object-fit: contain;
-  }
-
-  nav {
-    display: flex;
-    justify-content: center;
-    --background: rgba(255, 255, 255, 0.7);
-  }
-
-  svg {
-    width: 2em;
-    height: 3em;
-    display: block;
-  }
-
-  path {
-    fill: var(--background);
-  }
-
-  ul {
+<style lang="scss">
+  .container {
+    margin: auto;
+    text-align: center;
     position: relative;
-    padding: 0;
-    margin: 0;
-    height: 3em;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    list-style: none;
-    background: var(--background);
-    background-size: contain;
+    width: 100vw;
+    // height: 100vh;
   }
 
-  li {
-    position: relative;
-    height: 100%;
-  }
-
-  li.active::before {
-    --size: 6px;
-    content: '';
-    width: 0;
-    height: 0;
+  .scissors {
     position: absolute;
-    top: 0;
-    left: calc(50% - var(--size));
-    border: var(--size) solid transparent;
-    border-top: var(--size) solid var(--accent-color);
+    left: 50%;
+    top: 30rem;
+    transform: rotate(12deg) translate(-50%, -50%);
+    font-family: STIXGeneral;
+    font-size: 50rem;
+    opacity: 0.2;
+    line-height: 0;
   }
 
-  nav a {
-    display: flex;
-    height: 100%;
-    align-items: center;
-    padding: 0 1em;
-    color: var(--heading-color);
-    font-weight: 700;
-    font-size: 0.8rem;
+  .logo-name {
+    font-family: "Infini";
+    font-weight: 900;
+    font-size: 8rem;
+    line-height: 1.4;
+    text-align: center;
+    letter-spacing: 1.9rem;
+    margin: auto;
+    max-width: 95rem;
+    width: 88%;
+    border-bottom: 15px dashed rgba(246,236,227,0.2);
     text-transform: uppercase;
-    letter-spacing: 10%;
-    text-decoration: none;
-    transition: color 0.2s linear;
+    padding: 12rem 0 2rem 0;
+    display: inline-block;
   }
 
-  a:hover {
-    color: var(--accent-color);
+  .intro {
+    margin: 7rem auto;
+    max-width: 95rem;
+    width: 88%;
+    font-size: 1.6rem;
+    line-height: 1.5;
+    font-family: "Menoe Grotesque Pro";
+    font-weight: 400;
+    letter-spacing: 0.04rem;
+    text-align: justify;
   }
+
+  #site-main::after {
+    background: url(./images/noise.jpg);
+    content: "";
+    height: 100%;
+    left: 0px;
+    opacity: 0.02;
+    pointer-events: none;
+    position: absolute;
+    top: 0px;
+    width: 100%;
+    z-index: 201;
+  }
+
+  .noselect {
+    -webkit-user-select: none;
+    user-select: none;
+  }
+
 </style>
