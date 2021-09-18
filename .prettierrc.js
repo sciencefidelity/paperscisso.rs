c242ea0349cbb9b8dcc72f8a386370b3df257804
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2021-09-19 00:22:16 +0100

    Bring the environment up to date

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  arrowParens: 'avoid',
+  singleQuote: false,
+  printWidth: 90,
+  plugins: ['prettier-plugin-svelte'],
+  semi: false,
+  svelteSortOrder: 'options-styles-scripts-markup',
+  svelteStrictMode: false,
+  svelteBracketNewLine: true,
+  svelteIndentScriptAndStyle: true,
+  trailingComma: 'none'
+}
