46006f4589f65efafcf42fcdf51dcf055dbaadb1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-01-01 01:04:07 +0000

    build: add modules and configs for prettier, eslint and stylelint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,32 @@
 module.exports = {
   root: true,
-  parser: "@typescript-eslint/parser",
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-  plugins: ["svelte3", "@typescript-eslint"],
-  ignorePatterns: ["*.cjs"],
-  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
-  settings: {
-    "svelte3/typescript": () => require("typescript")
-  },
-  parserOptions: {
-    sourceType: "module",
-    ecmaVersion: 2019
-  },
   env: {
     browser: true,
     es2017: true,
     node: true
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier"
+  ],
+  plugins: ["svelte3", "@typescript-eslint"],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020
+  },
+  ignorePatterns: ["*.cjs", "*.scss"],
+  overrides: [{ files: ["*.svelte"], processor: "svelte3/svelte3" }],
+  rules: {
+    indent: ["error", 2],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "double"],
+    semi: ["error", "never"]
+  },
+  settings: {
+    "svelte3/typescript": () => require("typescript"),
+    "svelte3/ignore-styles": attributes =>
+      attributes.lang && attributes.lang.includes("scss")
   }
 }
