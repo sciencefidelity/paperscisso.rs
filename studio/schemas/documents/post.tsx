5f8f89d5b5220575474452e6e24288ecba3fda46
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-12 16:03:10 +0000

    build: set up prettier and reformat

--- studio/schemas/documents/post.tsx
+++ studio/schemas/documents/post.tsx
@@ -24,7 +24,7 @@ export default {
     {
       name: 'title',
       title: 'Title',
-      type: 'string',
+      type: 'string'
     },
     {
       name: 'slug',
@@ -32,52 +32,52 @@ export default {
       type: 'slug',
       options: {
         source: 'title',
-        maxLength: 96,
-      },
+        maxLength: 96
+      }
     },
     {
       name: 'author',
       title: 'Author',
       type: 'reference',
-      to: {type: 'author'},
+      to: { type: 'author' }
     },
     {
       name: 'mainImage',
       title: 'Main image',
       type: 'image',
       options: {
-        hotspot: true,
-      },
+        hotspot: true
+      }
     },
     {
       name: 'categories',
       title: 'Categories',
       type: 'array',
-      of: [{type: 'reference', to: {type: 'category'}}],
+      of: [{ type: 'reference', to: { type: 'tag' } }]
     },
     {
       name: 'publishedAt',
       title: 'Published at',
-      type: 'datetime',
+      type: 'datetime'
     },
     {
       name: 'body',
       title: 'Body',
-      type: 'blockContent',
-    },
+      type: 'portableText'
+    }
   ],
 
   preview: {
     select: {
       title: 'title',
       author: 'author.name',
-      media: 'mainImage',
+      media: 'mainImage'
     },
     prepare(selection) {
-      const {author} = selection
+      const { author } = selection
       return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
+        subtitle: author && `by ${author}`
       })
-    },
-  },
+    }
+  }
 }
