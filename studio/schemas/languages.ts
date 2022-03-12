94f36a396633e7404b73aedb72937eebb54ed323
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-12 22:28:46 +0000

    refactor: remove comments and change en to en-GB

--- studio/schemas/languages.ts
+++ studio/schemas/languages.ts
@@ -1,12 +1,5 @@
-// export interface Language {
-//   readonly name: string
-//   readonly title: string
-//   readonly isDefault?: boolean
-// }
-
 export const supportedLanguages = [
-  { name: 'en', title: 'English', isDefault: true },
+  { name: 'en-GB', title: 'English', isDefault: true },
   { name: 'cy', title: 'Welsh' }
 ]
-
 export const baseLanguage = supportedLanguages.find(l => l.isDefault)
