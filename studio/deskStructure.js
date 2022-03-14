74c7d2dbd308be1d18ea166e6150cf45c9a76abb
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-03-14 23:53:40 +0000

    feat: generate astro project for testing

--- studio/deskStructure.js
+++ studio/deskStructure.js
@@ -1,3 +1,7 @@
+import S from '@sanity/desk-tool/structure-builder'
 import * as Structure from '@sanity/document-internationalization/lib/structure'
 
-export default Structure.default
+export default () => {
+  const items = Structure.getFilteredDocumentTypeListItems()
+  return S.list().id('__root__').title('Content').items(items)
+}
