73dda7eaee76b1b7e4cea434f46f7b6f5a299759
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-05-20 23:46:35 +0100

    build: set up base config

--- alpha/next.config.js
+++ alpha/next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ["en", "cy"],
+    defaultLocale: "en"
+  },
+  images: {
+    domains: ["cdn.sanity.io"]
+  },
+  swcMinify: true
 }
 
 module.exports = nextConfig
