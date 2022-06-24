f14a46609a241857acc011f86f63efabe5ec29a0
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-24 20:22:01 -0230

    feat: scaffold stencil component project

---
+++ acre/src/components/acre-clock/acre-clock.tsx
@@ -0,0 +1,18 @@
+import { Component, h } from '@stencil/core';
+
+@Component({
+  tag: 'acre-clock',
+  styleUrl: 'acre-clock.css',
+  shadow: true,
+})
+export class AcreClock {
+
+  render() {
+    return (
+      <div>
+        Hi Matt!
+      </div>
+    );
+  }
+
+}
