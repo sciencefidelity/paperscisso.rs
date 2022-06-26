dd8240399b37b2707bdb164baf87f402e9736af2
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-25 23:31:52 -0100

    refactor: add word count in bottom corner

--- written/src/App.tsx
+++ written/src/App.tsx
@@ -5,9 +5,12 @@ import "react-quill/dist/quill.bubble.css"
 const App = () => {
   const [value, setValue] = useState("")
 
+  const wordCount = value.split(" ").length
+
   return (
     <div className="container">
       <ReactQuill theme="bubble" value={value} onChange={setValue} />
+      <div className="wordCount">{wordCount} words</div>
     </div>
   )
 }
