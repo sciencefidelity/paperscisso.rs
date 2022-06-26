9fc183d5e0c1b99d3624bf01ac34a301d109bfd7
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-25 23:51:25 -0100

    refactor: add default formats and update heading styles

--- written/src/App.tsx
+++ written/src/App.tsx
@@ -5,11 +5,31 @@ import "react-quill/dist/quill.bubble.css"
 const App = () => {
   const [value, setValue] = useState("")
 
+  const modules = {
+    toolbar: [
+      [{ header: [1, 2, 3, 4, false] }],
+      ["bold", "italic", "underline", "strike", "blockquote"],
+      [
+        { list: "ordered" },
+        { list: "bullet" },
+        { indent: "-1" },
+        { indent: "+1" }
+      ],
+      ["link", "image"],
+      ["clean"]
+    ]
+  }
+
   const wordCount = value.split(" ").length
 
   return (
     <div className="container">
-      <ReactQuill theme="bubble" value={value} onChange={setValue} />
+      <ReactQuill
+        theme="bubble"
+        modules={modules}
+        value={value}
+        onChange={setValue}
+      />
       <div className="wordCount">{wordCount} words</div>
     </div>
   )
