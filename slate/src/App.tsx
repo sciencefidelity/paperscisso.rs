f4ec4b15b4422db7be80ba05c0a6e1367904086a
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-06-28 00:30:13 +0100

    feat: render slate and add an event handler

--- slate/src/App.tsx
+++ slate/src/App.tsx
@@ -1,6 +1,38 @@
-import { FC } from "react"
+import { useState } from "react"
+import { createEditor, BaseEditor, Descendant } from "slate"
+import { Slate, Editable, withReact, ReactEditor } from "slate-react"
 
-const App: FC = () => {
-  return <div>Hello world!</div>
+type CustomElement = { type: "paragraph"; children: CustomText[] }
+type CustomText = { text: string }
+
+declare module "slate" {
+  interface CustomTypes {
+    Editor: BaseEditor & ReactEditor
+    Element: CustomElement
+    Text: CustomText
+  }
+}
+
+const initailValue = [
+  {
+    type: "paragraph",
+    children: [{ text: "A line of text in a paragraph." }],
+  }
+]
+
+const App = () => {
+  const [editor] = useState(() => withReact(createEditor()))
+  return (
+    <Slate editor={editor} value={initailValue}>
+      <Editable 
+        onKeyDown={event => {
+          if (event.key === "&") {
+            event.preventDefault()
+            editor.insertText("and")
+          }
+        }}
+      />
+    </Slate>
+  )
 }
 export default App
