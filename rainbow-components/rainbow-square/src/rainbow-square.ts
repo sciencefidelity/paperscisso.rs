5898ac0a9d754ad7c75aa738850afe22baa02655
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-07-14 17:24:33 +0100

    feat: start building new component library

---
+++ rainbow-components/rainbow-square/src/rainbow-square.ts
@@ -0,0 +1,45 @@
+import { html, css, LitElement } from "lit";
+import { customElement } from "lit/decorators.js";
+import { map } from "lit/directives/map.js";
+
+@customElement("rainbow-square")
+export class RainbowSquare extends LitElement {
+  squares = Array.from(Array(10).keys());
+
+  render() {
+    return html`<div id="canvas">
+      ${map(
+        this.squares,
+        (square) => html` <div
+          class="square-${square}"
+          style=${`
+            width: ${square * 10}%;
+            background: var(--color-${square});
+            z-index: ${-square};
+          `}
+        ></div>`
+      )}
+    </div>`;
+  }
+
+  static styles = css`
+    #canvas {
+      display: grid;
+      width: 500px;
+      height: 500px;
+      position: relative;
+    }
+    #canvas > div {
+      aspect-ratio: 1;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+    }
+  `;
+}
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "rainbow-square": RainbowSquare;
+  }
+}
