134df15390267dbeca6856bbab99264c656e9a02
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-20 13:14:59 +0100

    feat: add the base for a fully custom text editor

--- prosemirror-svelte/src/lib/EditorView.svelte
+++ prosemirror-svelte/src/lib/EditorView.svelte
@@ -1,68 +1,69 @@
 <script lang="ts">
-  import { onMount, onDestroy } from 'svelte';
-  import { EditorState } from 'prosemirror-state';
-  import { EditorView } from 'prosemirror-view';
-  import { Schema, DOMParser } from 'prosemirror-model';
-  import { schema } from 'prosemirror-schema-basic';
-  import { addListNodes } from 'prosemirror-schema-list';
-  import { exampleSetup } from 'prosemirror-example-setup';
+  import 'prosemirror-view/style/prosemirror.css'
+  import { history, redo, undo } from 'prosemirror-history'
+  import { keymap } from 'prosemirror-keymap'
+  import { Schema, type NodeSpec } from 'prosemirror-model'
+  import { EditorState } from 'prosemirror-state'
+  import { EditorView } from 'prosemirror-view'
+  import { onDestroy, onMount } from 'svelte'
+  import {
+    baseKeymap,
+    chainCommands,
+    createParagraphNear,
+    liftEmptyBlock,
+    newlineInCode,
+    splitBlock
+  } from 'prosemirror-commands'
 
-  let view: EditorView;
-  let editor: HTMLElement;
+  let view: EditorView
+  let editor: HTMLElement
+  let state: EditorState
 
-  const mySchema = new Schema({
-    nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
-    marks: schema.spec.marks
-  });
+  const nodes = {
+    doc: {
+      content: 'block+'
+    } as NodeSpec,
+
+    paragraph: {
+      content: 'inline*',
+      group: 'block',
+      parseDOM: [{ tag: 'p' }],
+      toDOM() {
+        return ['p', 0]
+      }
+    } as NodeSpec,
+
+    text: {
+      group: 'inline'
+    } as NodeSpec
+  }
+
+  const schema = new Schema({ nodes })
 
   onMount(() => {
-    view = new EditorView(
-      { mount: editor },
-      {
-        state: EditorState.create({
-          doc: DOMParser.fromSchema(mySchema).parse(
-            document.querySelector('#content')!
+    state = EditorState.create({
+      schema,
+      plugins: [
+        history(),
+        keymap({
+          Enter: chainCommands(
+            newlineInCode,
+            createParagraphNear,
+            liftEmptyBlock,
+            splitBlock
           ),
-          plugins: exampleSetup({ schema: mySchema })
-        })
-      }
-    );
-  });
+          'Mod-z': undo,
+          'Mod-y': redo
+        }),
+        keymap(baseKeymap)
+      ]
+    })
+    view = new EditorView({ mount: editor }, { state })
+  })
 
   onDestroy(() => {
-    view && view.destroy();
-  });
+    view && view.destroy()
+  })
 </script>
 
-<div bind:this={editor} on:focus on:blur on:keydown />
-<div style="display: none" id="content">
-  <h3>Hello ProseMirror</h3>
-  <p>This is editable text. You can focus it and start typing.</p>
-  <p>
-    To apply styling, you can select a piece of text and manipulate its styling
-    from the menu. The basic schema supports <em>emphasis</em>,
-    <strong>strong text</strong>,
-    <a href="http://marijnhaverbeke.nl/blog">links</a>, <code>code font</code>,
-    and <img src="/img/smiley.png" /> images.
-  </p>
-  <p>
-    Block-level structure can be manipulated with key bindings (try ctrl-shift-2
-    to create a level 2 heading, or enter in an empty textblock to exit the
-    parent block), or through the menu.
-  </p>
-  <p>
-    Try using the “list” item in the menu to wrap this paragraph in a numbered
-    list.
-  </p>
-</div>
-
-<!-- <style lang="postcss" global>
-  .prosemirror {
-    border: 1px solid #ccc;
-    height: 100%;
-    padding: 0.5rem 1rem;
-    p {
-      margin-block: 0.5rem;
-    }
-  }
-</style> -->
+<div bind:this={editor} />
--- prosemirror-svelte/src/routes/index.svelte
+++ prosemirror-svelte/src/routes/index.svelte
@@ -1,11 +1,11 @@
 <script context="module" lang="ts">
-  export const prerender = true;
+  export const prerender = true
 </script>
 
 <script lang="ts">
-  import '../../node_modules/prosemirror-example-setup/style/style.css';
-  import '../app.css';
-  import EditorView from '$lib/EditorView.svelte';
+  import '../../node_modules/prosemirror-example-setup/style/style.css'
+  import '../app.css'
+  import EditorView from '$lib/EditorView.svelte'
 </script>
 
 <svelte:head>
@@ -19,22 +19,16 @@
   </section>
 </main>
 
-<style global>
+<!-- <style global>
   .main {
     display: grid;
     align-items: start;
-    /* justify-content: center; */
-    /* height: 100vh; */
   }
 
   .editor {
     margin: 0 auto;
     width: 85%;
     height: 30rem;
-  }
-
-  #editor,
-  .editor {
     background: white;
     color: black;
     background-clip: padding-box;
@@ -43,40 +37,4 @@
     padding: 5px 0;
     margin-bottom: 23px;
   }
-
-  .ProseMirror-menubar {
-    border-top-left-radius: inherit;
-    border-top-right-radius: inherit;
-    position: relative;
-    min-height: 1em;
-    color: #666;
-    padding: 1px 6px;
-    top: 0;
-    left: 0;
-    right: 0;
-    border-bottom: 1px solid silver;
-    background: white;
-    z-index: 10;
-    -moz-box-sizing: border-box;
-    box-sizing: border-box;
-    overflow: visible;
-  }
-
-  .ProseMirror-menuitem {
-    margin-right: 3px;
-    display: inline-block;
-  }
-
-  .ProseMirror-menuseparator {
-    border-right: 1px solid #ddd;
-    margin-right: 3px;
-  }
-
-  .ProseMirror-icon {
-    display: inline-block;
-    line-height: 0.8;
-    vertical-align: -2px;
-    padding: 2px 8px;
-    cursor: pointer;
-  }
-</style>
+</style> -->
