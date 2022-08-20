e84da65de68fffb6d7508442db7b236a702eddf1
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-20 16:19:53 +0100

    feat: add some decorations in a plugin

--- prosemirror-svelte/src/lib/EditorView.svelte
+++ prosemirror-svelte/src/lib/EditorView.svelte
@@ -1,11 +1,4 @@
 <script lang="ts">
-  import 'prosemirror-view/style/prosemirror.css'
-  import { history, redo, undo } from 'prosemirror-history'
-  import { keymap } from 'prosemirror-keymap'
-  import { Schema, type NodeSpec } from 'prosemirror-model'
-  import { EditorState } from 'prosemirror-state'
-  import { EditorView } from 'prosemirror-view'
-  import { onDestroy, onMount } from 'svelte'
   import {
     baseKeymap,
     chainCommands,
@@ -14,6 +7,13 @@
     newlineInCode,
     splitBlock
   } from 'prosemirror-commands'
+  import { history, redo, undo } from 'prosemirror-history'
+  import { keymap } from 'prosemirror-keymap'
+  import { Node, Schema, type NodeSpec } from 'prosemirror-model'
+  import { EditorState, Plugin, PluginKey } from 'prosemirror-state'
+  import { Decoration, DecorationSet, EditorView } from 'prosemirror-view'
+  import 'prosemirror-view/style/prosemirror.css'
+  import { onDestroy, onMount } from 'svelte'
 
   let view: EditorView
   let editor: HTMLElement
@@ -29,7 +29,7 @@
       group: 'block',
       parseDOM: [{ tag: 'p' }],
       toDOM() {
-        return ['p', 0]
+        return ['div', 0]
       }
     } as NodeSpec,
 
@@ -40,9 +40,76 @@
 
   const schema = new Schema({ nodes })
 
+  let doc = schema.node('doc', null, [
+    schema.node('paragraph', null, [
+      schema.text('This is the first paragraph.')
+    ]),
+    schema.node('paragraph', null, [
+      schema.text('This is the second paragraph.')
+    ]),
+    schema.node('paragraph', null, [
+      schema.text('This is the third paragraph.')
+    ])
+  ])
+
+  // const grabHandle = () => {
+  //   return new Plugin({
+  //     key: new PluginKey('grabHandle'),
+  //     state: {
+  //       init: (_, { doc }) => grabHandleDecoration(doc),
+  //       apply: (tr, old) => (tr.docChanged ? grabHandleDecoration(tr.doc) : old)
+  //     },
+  //     props: {
+  //       decorations() {
+  //         return this.getState(doc)
+  //       }
+  //     }
+  //   })
+  // }
+
+  const grabHandleIcon = () => {
+    const element = document.createElement('div')
+    element.className = 'grab-handle'
+    element.innerHTML = '🌕'
+    return element
+  }
+
+  const foldableIcon = () => {
+    const element = document.createElement('div')
+    element.className = 'foldable'
+    element.innerHTML = '🌍'
+    return element
+  }
+
+  const bulletIcon = () => {
+    const element = document.createElement('div')
+    element.className = 'bullet'
+    element.innerHTML = '🪐'
+    return element
+  }
+
+  const emojiPlugin = new Plugin({
+    props: {
+      decorations() {
+        const decorators: Decoration[] = []
+        doc.descendants((node, pos) => {
+          if (!node.isBlock) return
+          decorators.push(Decoration.widget(pos + 1, grabHandleIcon()))
+          decorators.push(Decoration.widget(pos + 1, foldableIcon()))
+          decorators.push(Decoration.widget(pos + 1, bulletIcon()))
+        })
+        return DecorationSet.create(doc, decorators)
+      }
+    }
+  })
+
   onMount(() => {
+    // const content = document.querySelector('#content') as HTMLDivElement
+
     state = EditorState.create({
       schema,
+      // doc: DOMParser.fromSchema(schema).parse(content!),
+      doc,
       plugins: [
         history(),
         keymap({
@@ -55,7 +122,8 @@
           'Mod-z': undo,
           'Mod-y': redo
         }),
-        keymap(baseKeymap)
+        keymap(baseKeymap),
+        emojiPlugin
       ]
     })
     view = new EditorView({ mount: editor }, { state })
@@ -67,3 +135,25 @@
 </script>
 
 <div bind:this={editor} />
+
+<div id="content" class="output">
+  <p>This is the first paragraph.</p>
+  <p>This is the second paragraph.</p>
+  <p>This is the third paragraph.</p>
+</div>
+
+<style global lang="postcss">
+  .output {
+    display: none;
+    font-family: Georgia, 'Times New Roman', Times, serif;
+    padding: 0.5rem 1.5rem;
+    background-color: hsl(0, 0%, 97%);
+  }
+
+  .grab-handle,
+  .foldable,
+  .bullet {
+    display: inline-block;
+    margin-inline: 0.25rem;
+  }
+</style>
