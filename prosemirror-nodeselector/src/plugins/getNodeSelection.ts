821710726b84bec678419c325cb9eabdbc5043ac
sciencefidelity <32623301+sciencefidelity@users.noreply.github.com>
2022-08-31 18:03:51 +0100

    refactor: prettify

--- prosemirror-nodeselector/src/plugins/getNodeSelection.ts
+++ prosemirror-nodeselector/src/plugins/getNodeSelection.ts
@@ -1,22 +1,13 @@
 import { NodeSelection, Plugin, EditorState } from 'prosemirror-state'
 import { DecorationSet, Decoration, EditorView } from 'prosemirror-view'
-import type { NodePos } from 'src/types'
 
-/**
- * a non-atom node needs to be selected by pressing the command key
- * after expansion, click on the container node will be selected
- */
 export const getNodeSelection = () =>
   new Plugin({
     props: {
-      decorations: state => {
-        return getFocusNodeDecorationSet(state)
-      },
+      decorations: state => getFocusNodeDecorationSet(state),
       handleClick(view, _pos, event) {
         const clickedNode = getNodeByEvent(view, event)
-        if (clickedNode === null) {
-          return false
-        }
+        if (clickedNode === null) return false
         const { node, pos } = clickedNode
         if (!event.shiftKey && node.isBlock && !node.isTextblock && !node.isAtom) {
           const tr = view.state.tr
@@ -31,7 +22,7 @@ export const getNodeSelection = () =>
 
 const getFocusNodeDecorationSet = (state: EditorState) => {
   const { selection } = state
-  // if (!(sel instanceof NodeRangeSelection)) return DecorationSet.empty
+  // if (!(selection instanceof NodeRangeSelection)) return DecorationSet.empty
   const decorations: Decoration[] = []
   const parent = selection.$from.parent
   const parentPos = selection.$from.start(selection.$from.depth)
@@ -45,14 +36,11 @@ const getFocusNodeDecorationSet = (state: EditorState) => {
   return DecorationSet.create(state.doc, decorations)
 }
 
-const getNodeByEvent = (view: EditorView, event: MouseEvent): NodePos | null => {
+const getNodeByEvent = (view: EditorView, event: MouseEvent) => {
   const pos = view.posAtCoords({ left: event.clientX, top: event.clientY })
   if (!pos) return null
   if (pos.inside === -1) return null
   const $pos = view.state.doc.resolve(pos.inside)
   if (!$pos.nodeAfter) return null
-  return {
-    node: $pos.nodeAfter,
-    pos: $pos.before($pos.depth + 1)
-  }
+  return { node: $pos.nodeAfter, pos: $pos.before($pos.depth + 1) }
 }
